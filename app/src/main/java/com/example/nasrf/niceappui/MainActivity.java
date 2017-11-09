7605f2fd076590198d6468a87dfaa7549cfc12f1
Nasr Mohamed <32339910+nasrmohamedfcis@users.noreply.github.com>
2017-11-09 14:14:58 +0200

    Final

--- app/src/main/java/com/example/nasrf/niceappui/MainActivity.java
+++ app/src/main/java/com/example/nasrf/niceappui/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.nasrf.niceappui;
 
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDatePicker;
+    TextView txtDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDatePicker=(Button) findViewById(R.id.btnDatePicker);
+        txtDate = (TextView) findViewById(R.id.txtDate);
+
     }
+
+    public void onClick(View v) {
+
+
+    }
+
+
+
 }
